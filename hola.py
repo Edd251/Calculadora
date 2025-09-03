69e224ac28cedc1dbc705f3dcd1573fe570dfb49
Edd251 <edwardamaya400@gmail.com>
2025-09-02 22:24:26 -0500

    Se agrego el saludo en frances

--- hola.py
+++ hola.py
@@ -4,6 +4,9 @@ def saludar (nombre):
 def saludarIngles (nombre): 
     print("Hi, "+ nombre)
 
+def saludarFrances (nombre): 
+    print("Bonjour, "+ nombre)
 
 saludar ("Edwar")
 saludarIngles ("Edwar")
+saludarFrances ("Edwar")
